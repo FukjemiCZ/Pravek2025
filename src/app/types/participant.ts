84a67410992a5658b1b98e46b671c5a0d5df0802
FukjemiCZ <github@fukjemi.cz>
2026-02-21 14:23:25 +0100

    monorepo

--- src/app/types/participant.ts
+++
@@ -1,8 +0,0 @@
-export interface Participant {
-    name: string;
-    registration_order: number;
-    dogs: string[];
-    paid: boolean;
-    isOnStartList: boolean;
-    registration_date: string;
-  }
