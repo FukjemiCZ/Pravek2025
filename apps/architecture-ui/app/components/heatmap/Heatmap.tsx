364d599f32acdcb58e330196f1e7a2f3b25e2ee7
FukjemiCZ <github@fukjemi.cz>
2026-02-21 15:21:04 +0100

    arch ui map

--- apps/architecture-ui/app/components/heatmap/Heatmap.tsx
+++ apps/architecture-ui/app/components/heatmap/Heatmap.tsx
@@ -3,34 +3,36 @@
 import { Table, TableBody, TableCell, TableHead, TableRow } from "@mui/material";
 
 export default function Heatmap({ heatmap }: any) {
-  const { domains, epics, matrix } = heatmap;
+    const { domains, epics, matrix } = heatmap;
 
-  const map = new Map(
-    matrix.map((x: any) => [`${x.domainId}-${x.epicId}`, x.count])
-  );
-
-  return (
-    <Table>
-      <TableHead>
-        <TableRow>
-          <TableCell>Domain / Epic</TableCell>
-          {epics.map((e: any) => (
-            <TableCell key={e.id}>{e.id}</TableCell>
-          ))}
-        </TableRow>
-      </TableHead>
-      <TableBody>
-        {domains.map((d: any) => (
-          <TableRow key={d.id}>
-            <TableCell>{d.id}</TableCell>
-            {epics.map((e: any) => (
-              <TableCell key={e.id}>
-                {map.get(`${d.id}-${e.id}`) || 0}
-              </TableCell>
-            ))}
-          </TableRow>
-        ))}
-      </TableBody>
-    </Table>
-  );
+    const map = new Map<string, number>(
+        matrix.map((x: any) => [
+            `${x.domainId}-${x.epicId}`,
+            Number(x.count)
+        ])
+    );
+    return (
+        <Table>
+            <TableHead>
+                <TableRow>
+                    <TableCell>Domain / Epic</TableCell>
+                    {epics.map((e: any) => (
+                        <TableCell key={e.id}>{e.id}</TableCell>
+                    ))}
+                </TableRow>
+            </TableHead>
+            <TableBody>
+                {domains.map((d: any) => (
+                    <TableRow key={d.id}>
+                        <TableCell>{d.id}</TableCell>
+                        {epics.map((e: any) => (
+                            <TableCell key={e.id}>
+                                {Number(map.get(`${d.id}-${e.id}`) ?? 0)}
+                            </TableCell>
+                        ))}
+                    </TableRow>
+                ))}
+            </TableBody>
+        </Table>
+    );
 }
